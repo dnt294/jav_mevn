98bc6775c811dcac5ceb5e02bcfca30c284c7930
Nguyen Thanh Dat <kaanoo2904@gmail.com>
2018-04-20 13:22:00 +0700

    [Backend] index and create for lessons

--- server/src/routes/lessonRoutes.ts
+++ server/src/routes/lessonRoutes.ts
@@ -4,12 +4,16 @@ const lessonRoutes = express.Router();
 import Lesson from '@models/lesson';
 
 lessonRoutes.route('/').get((req, res) => {
-  res.json({
-    status: 'ok fine'
-  })
+  Lesson.find({}, (err, lessons) => {
+    res.json(lessons);
+  });
 });
 
 lessonRoutes.route('/').post((req, res) => {
+  const lesson = new Lesson(req.body.lesson);
+  Lesson.create(req.body.lesson, (err, lesson) => {
+    res.json(lesson);
+  });
 });
 
 export default lessonRoutes;
